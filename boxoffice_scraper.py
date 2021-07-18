482ed97bd26994725846ee6105ff6e27a7b9c667
Hiromisa <57719234+Hiromisa@users.noreply.github.com>
2021-07-17 19:54:42 -0700

    Update boxoffice_scraper.py

--- boxoffice_scraper.py
+++ boxoffice_scraper.py
@@ -1,47 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-# In[26]:
-
-
 url = 'https://www.boxofficemojo.com/weekly/?ref_=bo_nb_ml_secondarytab'
 df = pd.io.html.read_html(url)
 
-
-# In[27]:
-
-
 df = df[0]
-
-
-# In[36]:
-
 
 df = df.drop(columns=['Genre','Budget','Running Time']).rename(columns={'%± LW' : 'Top10 Gross Change','%± LW.1' : 'Overall Grross Change'}) 
 
-
-# In[38]:
-
-
 df.to_csv('WeeklyBoxoffice.csv', index=False)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
